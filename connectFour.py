37a751de213685e8bc1ec51035beb4813703f592
Lildhansen <madslh@yahoo.com>
2021-03-02 11:54:31 +0100

    made startup and displays board/background

--- connectFour.py
+++ connectFour.py
@@ -1,21 +1,33 @@
 import pygame,time
 
 
-
 pygame.init()
 logo = pygame.image.load("billeder/logo.png")
 pygame.display.set_icon(logo)
-window = pygame.display.set_mode((640,480))
+window = pygame.display.set_mode((640,480)) #each row = 91,43
 pygame.display.set_caption("Connect Four")
 
-background = pygame.image.load("billeder/board.png")
+#class Background(pygame.sprite.Sprite):
+ #   def __init__(self, image_file, location):
+  #      pygame.sprite.Sprite.__init__(self)  #call Sprite initializer
+   #     self.image = pygame.image.load(image_file)
+    #    self.rect = self.image.get_rect()
+     #   self.rect.left, self.rect.top = location
+
+bg = pygame.image.load('billeder/bg.png')
 
 running = True
 
+pygame.draw.rect(window,(255,255,255),(0,0,0,0))
+
 while running:
-    for event in pygame.event.get():
+    pygame.display.update()
+    window.blit(bg,[0,0])
+
+    for event in pygame.event.get(): 
+        
         if event.type == pygame.QUIT:
             running = False
-        window.blit(background,(0,0))   
+        
 
 pygame.quit()
