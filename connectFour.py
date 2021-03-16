6dff6bbff12a6d7821c9a55ba56f5d7c97cf4523
Lildhansen <madslh@yahoo.com>
2021-03-16 08:19:06 +0100

    added a lot to the gameboard to keep track of pieces

--- connectFour.py
+++ connectFour.py
@@ -1,6 +1,6 @@
 #take from chess.py (backup) - so splite the board into 7 parts and look for where the mouse is clicked and not collision
 # Make an array/dictionary/object (class) - probably class, to keep track of how many pieces are in each column.
-# make function to add to column - that takes which column (starting from 0) as parameter
+# make function to add to column - that takes which column (starting from 1) as parameter (0 is invalid)
 # When 1 column is full make it so you cant put any more pieces in
 
 
@@ -21,16 +21,38 @@
 
 import pygame,time
 
+#sizes
 COLUMN_WIDTH = 91.43
+GAMEBOARD_WIDTH = 640
+GAMEBOARD_HEIGHT = 480
+
 SENTINEL_POSITION = (1000) #a position out of bounds
+COLUMNS = 7
+ROW = 6
+
 
 pygame.init()
 logo = pygame.image.load("billeder/logo.png")
 pygame.display.set_icon(logo)
-window = pygame.display.set_mode((640,480+40)) #each column = 91,43 #højden af billedet er 480 - tilføjer 40 for at man kan se hvis tur det er
+window = pygame.display.set_mode((GAMEBOARD_WIDTH,GAMEBOARD_HEIGHT+40)) #each column = 91,43 #højden af billedet er 480 - tilføjer 40 for at man kan se hvis tur det er
 pygame.display.set_caption("Connect Four")
 
 bg = pygame.image.load('billeder/bg.png')
+
+def codePosToGamePos(codePos): #takes list as parameter
+	gamePos = codePos.copy()
+	return [(i-1)*COLUMN_WIDTH for i in gamePos]
+
+def gamePosToCodePos(gamePos):
+	codePos = list(gamePos)
+	count = 1
+	if codePos[1] > GAMEBOARD_HEIGHT:
+		return 0
+	while codePos[0] > COLUMN_WIDTH:
+		codePos[0] -= COLUMN_WIDTH
+		count += 1
+	return count
+
 
 def drawBoard(): #draws the squares of the board + the upper bounds
 	columns = []
@@ -41,9 +63,35 @@ def drawBoard(): #draws the squares of the board + the upper bounds
 	return columns,topOfColumn
 hitboxes,topOfColumns = drawBoard()
 
+#helper for Gameboard self.piecesInGameboardArray:
+a = [[0]*8 for y in range(7)]
+i = 0
+while i < 7:
+    a[0][i] = SENTINEL_VALUE
+    a[i][0] = SENTINEL_VALUE
+    i += 1
+a[0][7] = SENTINEL_VALUE 
 
+class Gameboard():
+	def __init__(self):
+		#this value counts how many brikker that is in each of the columns (1-7 columns) - (0-6 pieces in each column)
+		self.columnCounter = [SENTINEL_POSITION,0,0,0,0,0,0,0]#first value is sentiel, since columns start from 1 - rest have 0 in them to begin with
+		# 0 = empty 1 = red, 2 = yellow SENTINEL_VALUE = invalid Position
+		self.piecesInGameboardArray = a
+	def addToColumnCounter(column):
+		self.columnCounter[column] += 1
+	def addToPiecesInGameboardArray(column,isRed):
+		pass
+		#find what position to place it with columnCounter(maybe call it in this func)
+		#change value of piecesInGameboardArray
+		#call func that adds visually to board
+		#manage turns
+
+
+
+
+gameboard = Gameboard()
 #21 af hver - lav alle brikkerne på forhånd i lister - kombiner med columnIsNotFull()
-
 class PlayerTurnDisplay(): #create 2 instances - with each color, and make the other one invisible/have white color when not their turn / change its position to out of bounds
 	def __init__(self,isRed,player):
 		self.textbox = pygame.draw.rect(window,(0,0,0),(0,480,640,40))
@@ -70,12 +118,15 @@ def columnOfMouseclick(): #returns false if it is an invalid position - else ret
 			return index
 	return False
 
-def columnIsNotFull(indexOfColumn): #returns False if the column is full and true if it is not
-	for x in topOfColumns:
-		print(x.colliderect())
-	return True
-	#hvis en brik matcher med en topOfColumns	
 
+
+def validColumnSelected(column):
+	return bool(column)
+
+def columnNotFull(column):
+	if gameboard.columnCounter[column] < 7:
+		return True
+	return False
 
 def addPiece():
 	#check mouse position - er det korrekt:
@@ -96,11 +147,16 @@ while running:
 		if event.type == pygame.QUIT:
 			running = False
 		if event.type == pygame.MOUSEBUTTONUP:
-			pos = pygame.mouse.get_pos()
-			clickedColumn = columnOfMouseclick()
-			x = columnIsNotFull(clickedColumn)
-			if clickedColumn and x:
-				addPiece()
+			clickedPos = pygame.mouse.get_pos()
+			column = gamePosToCodePos(clickedPos)
+			if (validColumnSelected(column) and columnNotFull(column)):
+				gameboard.addToColumnCounter(column)
+				
+			#pos = pygame.mouse.get_pos()
+			#clickedColumn = columnOfMouseclick()
+			#x = columnIsNotFull(clickedColumn)
+			#if clickedColumn and x:
+			#	addPiece()
 	pygame.display.update()
 
 pygame.quit()
